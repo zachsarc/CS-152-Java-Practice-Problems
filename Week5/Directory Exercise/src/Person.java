ab0e02839dca0b51451f00dbe20f6213dca01b1e
Zach Lariccia <sadboigoon@gmail.com>
2025-06-22 13:19:22 -0400

    Added new exercise for Week 5: OOP concepts

--- Week5/Directory Exercise/src/Person.java
+++ Week5/Directory Exercise/src/Person.java
@@ -47,21 +47,21 @@ public static void main(String[] args){
         Faculty faculty = new Faculty("Chris Lariccia" ,"123 Berry Ln","123-456-7890","124yoohoo@gmail.com","Rescue 1 Pest Control",100000000,System.currentTimeMillis() - 172800000L, 5400,"CEO");
         Staff staff = new Staff("Zach Lariccia" ,"123 Berry Ln","123-456-7890","124yoohoo@gmail.com","Rescue 1 Pest Control",100000000,System.currentTimeMillis() - 172800000L);
         System.out.println("Person");
-        System.out.println("Name:" + person.getName());
+        System.out.println("Name: " + person.getName());
         System.out.println("Address: " + person.getAddress());
         System.out.println("Phone: " + person.getPhone());
         System.out.println("Email: " + person.getemail());
 
     System.out.println();
     System.out.println("Student");
-    System.out.println("Name:" + student.getName());
+    System.out.println("Name: " + student.getName());
     System.out.println("Address: " + student.getAddress());
     System.out.println("Phone: " + student.getPhone());
     System.out.println("Email: " + student.getemail());
 
     System.out.println();
     System.out.println("Employee");
-    System.out.println("Name:" + employee.getName());
+    System.out.println("Name: " + employee.getName());
     System.out.println("Address: " + employee.getAddress());
     System.out.println("Phone: " + employee.getPhone());
     System.out.println("Email: " + employee.getemail());
@@ -71,7 +71,7 @@ public static void main(String[] args){
 
     System.out.println();
     System.out.println("Faculty");
-    System.out.println("Name" + faculty.getName());
+    System.out.println("Name: " + faculty.getName());
     System.out.println("Address: " + faculty.getAddress());
     System.out.println("Phone: " + faculty.getPhone());
     System.out.println("Email: " + faculty.getemail());
@@ -83,7 +83,7 @@ public static void main(String[] args){
 
     System.out.println();
     System.out.println("Staff");
-    System.out.println("Name" + staff.getName());
+    System.out.println("Name: " + staff.getName());
     System.out.println("Address: " + faculty.getAddress());
     System.out.println("Phone: " + faculty.getPhone());
     System.out.println("Email: " + faculty.getemail());
