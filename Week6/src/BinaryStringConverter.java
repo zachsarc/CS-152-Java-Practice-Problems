b9f6c4b810582ddf2606ea2549f441cfb2fed8c7
Zach Lariccia <sadboigoon@gmail.com>
2025-06-27 14:40:17 -0400

    Added binary String to a binary decimal problem to Week 6

---
+++ Week6/src/BinaryStringConverter.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class BinaryStringConverter {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            System.out.println("Enter the String you would like to convert: ");
+            String userInput = sc.nextLine();
+
+            try {
+                double result = thisToThat(userInput);
+                System.out.println("Your converted value is: " + result);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Not a binary number");
+            }
+        } while (true);
+    }
+
+    public static double thisToThat (String inp) throws NumberFormatException{
+        for (char c: inp.toCharArray()) {
+            if (c != '0' && c != '1') {
+                throw new NumberFormatException();
+            }
+        }
+        return Double.parseDouble(inp);
+    }
+}
--- Week6/src/TheOneHundred.java
+++ Week6/src/TheOneHundred.java
@@ -1,12 +1,4 @@
 import java.util.Scanner;
-/**
-(ArrayIndexOutOfBoundsException) Write a program that meets the
- following requirements:
- ■ ■ Creates an array with 100 randomly chosen integers.
- ■ ■ Prompts the user to enter the index of the array, then displays the
- corresponding element value. If the specified index is out of bounds, display the
- message "Out of Bounds".
- */
 
 public class TheOneHundred {
 
