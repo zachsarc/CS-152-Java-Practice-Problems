e3814bd92673c9fa2d1ef3801cdd116edfba247e
Zach Lariccia <sadboigoon@gmail.com>
2025-06-28 12:02:26 -0400

    Added a new week 6 problem, counting words, characters, and lines in a .txt document specified by an abs path from the user

--- Week6/src/FileAnalyzer.java
+++ Week6/src/FileAnalyzer.java
@@ -2,7 +2,7 @@
  * (Count characters, words, and lines in a file) Write a program that will count
  * the number of characters, words, and lines in a file. Words are separated by
  * whitespace characters. The file name should be passed as a command-line
- * argument, as shown in Figure 12.13.
+ * argument.
  */
 
 import java.io.File;
