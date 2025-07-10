04912ff01a43a545b6c9e09d485bbac02c806ac8
Zach Lariccia <sadboigoon@gmail.com>
2025-07-10 19:36:04 -0400

    Added new Week 8

---
+++ Week8/src/ReversedString.java
@@ -0,0 +1,22 @@
+ import java.util.Stack;
+
+    public class ReversedString {
+        public static void main(String[] args) {
+            System.out.println(reverseString("hello"));
+        }
+
+        public static String reverseString(String input) {
+            Stack<Character> stack = new Stack<>();
+            char[] charArray = input.toCharArray();
+            String reversed = "";
+            for (char c : charArray) {
+                stack.push(c);
+                System.out.println("Stack being built: " + stack);
+            }
+            while (!stack.isEmpty()) {
+                char r = stack.pop();
+                reversed += r;
+            }
+            return reversed;
+        }
+    }
---
+++ Week8/src/ValidPar.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+
+public class ValidPar {
+    public static void main(String[] args) {
+        ValidPar test = new ValidPar();
+        System.out.println(test.isValid("()[]{}"));
+        System.out.println(test.isValid("([)]"));
+        System.out.println(test.isValid("{[]}"));
+        System.out.println(test.isValid("((())"));
+    }
+
+    public boolean isValid (String par) {
+        Stack<Character> stack = new Stack<>();
+
+            for (char c : par.toCharArray()) {
+                if (c == '(' || c == '{' || c == '[') {
+                    stack.push(c);
+                } else {
+                    if (stack.isEmpty()) return false;
+
+                    char top = stack.pop();
+
+                    if ((c == ')') && top != '(') return false;
+                    if ((c == '}') && top != '{') return false;
+                    if ((c == ']') && top != '[') return false;
+                }
+            }
+            return stack.isEmpty();
+    }
+}
